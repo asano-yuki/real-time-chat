95bbe1117c8a5bcb6564a89b7d49c50fbf15da93
asano-yuki <asanoyuuki0523@gmail.com>
2023-05-31 19:58:38 +0900

    Prepare front development environment

---
+++ front/.eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'comma-dangle': [
+			'error',
+			'never'
+		],
+		'no-trailing-spaces': 2,
+		'eol-last': 'error',
+		'no-multiple-empty-lines': ['error', { 'max': 1 }],
+		'@typescript-eslint/no-empty-function': [
+			'off'
+		],
+		'@typescript-eslint/no-var-requires': [
+			'off'
+		]
+	}
+}
